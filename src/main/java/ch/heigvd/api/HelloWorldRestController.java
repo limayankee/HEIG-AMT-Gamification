392d5726c4e7dcb005bc87f38307a20c912b3c9d
Julien Leroy <julien.leroy@limayankee.com>
2017-01-20 15:35:09 +0100

    up

---
+++ src/main/java/ch/heigvd/api/HelloWorldRestController.java
@@ -0,0 +1,25 @@
+package ch.heigvd.api;
+
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+public class HelloWorldRestController {
+
+
+
+    //-------------------Retrieve All Users--------------------------------------------------------
+
+    @RequestMapping(value = "/test/", method = RequestMethod.GET)
+    public ResponseEntity<String> test() {
+        String rtn = "rtn";
+
+        return new ResponseEntity<String>(rtn, HttpStatus.OK);
+    }
+
+}
