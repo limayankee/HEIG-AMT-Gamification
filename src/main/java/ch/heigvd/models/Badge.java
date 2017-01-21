8781450ab3f73dfd3d7514abeb59512a92165e5a
Loic Serafin <loicserafin@gmail.com>
2017-01-21 11:03:05 +0100

    test hibernate badge

--- src/main/java/ch/heigvd/models/Badge.java
+++ src/main/java/ch/heigvd/models/Badge.java
@@ -5,14 +5,62 @@ import javax.persistence.Entity;
 import javax.persistence.Id;
 import javax.persistence.Table;
 
-/**
- * @author jfleroy
- */
 @Entity
 @Table(name = "badges")
-public class Badge
-{
-    @Id
-    @Column(name = "id")
-    private int id;
+public class Badge {
+	@Id
+	@Column(name = "id", nullable = false, unique = true)
+	private int id;
+
+	@Column(name = "name", nullable = false)
+	private String name;
+
+	@Column(name = "image", nullable = false)
+	private String image;
+
+	@Column(name = "count")
+	private int count;
+
+	@Column(name = "repeatable", nullable = false)
+	private boolean repeatable;
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getImage() {
+		return image;
+	}
+
+	public void setImage(String image) {
+		this.image = image;
+	}
+
+	public int getCount() {
+		return count;
+	}
+
+	public void setCount(int count) {
+		this.count = count;
+	}
+
+	public boolean isRepeatable() {
+		return repeatable;
+	}
+
+	public void setRepeatable(boolean repeatable) {
+		this.repeatable = repeatable;
+	}
 }
