532cb5d73d5615383528c99a5ea0205cc712594d
matthieuVillard <matthieu.villard@heig-vd.ch>
2017-01-21 11:19:00 +0100

    authentication from database

--- src/main/java/ch/heigvd/GamificationApplication.java
+++ src/main/java/ch/heigvd/GamificationApplication.java
@@ -1,10 +1,32 @@
 package ch.heigvd;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
+import org.springframework.core.env.Environment;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
 
+@Configuration
+@Import({ SecurityConfiguration.class })
 @SpringBootApplication
 public class GamificationApplication {
+
+	@Autowired
+	private Environment env;
+
+	@Bean(name = "dataSource")
+	public DriverManagerDataSource dataSource() {
+
+		DriverManagerDataSource driverManagerDataSource = new DriverManagerDataSource();
+		driverManagerDataSource.setDriverClassName("com.mysql.jdbc.Driver");
+		driverManagerDataSource.setUrl(env.getProperty("spring.datasource.url"));
+		driverManagerDataSource.setUsername(env.getProperty("spring.datasource.username"));
+		driverManagerDataSource.setPassword(env.getProperty("spring.datasource.password"));
+		return driverManagerDataSource;
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(GamificationApplication.class, args);
