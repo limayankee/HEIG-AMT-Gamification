c400db1647c0427eae7905c36e9c4599dd28a76e
matthieuVillard <matthieu.villard@heig-vd.ch>
2017-01-21 16:20:39 +0100

    saving application on authentication

--- src/main/java/ch/heigvd/SecurityConfiguration.java
+++ src/main/java/ch/heigvd/SecurityConfiguration.java
@@ -1,17 +1,27 @@
 package ch.heigvd;
 
+import ch.heigvd.dao.ApplicationRepository;
+import ch.heigvd.filter.AuthenticationFilter;
+import ch.heigvd.models.Application;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
 
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import javax.sql.DataSource;
+import java.io.IOException;
 
 @Configuration
 @EnableWebSecurity
@@ -21,6 +31,9 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
 	@Autowired
 	DataSource dataSource;
+
+	@Autowired
+	private ApplicationRepository applicationRepository;
 
 	@Autowired
 	public void configAuthentication(AuthenticationManagerBuilder auth) throws Exception {
@@ -42,6 +55,8 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 				.antMatchers("/**").hasAuthority("APPLICATION")
 				.and().httpBasic().realmName(REALM).authenticationEntryPoint(getBasicAuthEntryPoint())
 				.and().sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);//We don't need sessions to be created.
+
+		http.addFilter(new AuthenticationFilter(authenticationManager(), applicationRepository));
 	}
 
 	@Bean
@@ -54,5 +69,6 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 	public void configure(WebSecurity web) throws Exception {
 		web.ignoring()
 				.antMatchers(HttpMethod.OPTIONS, "/**");
+
 	}
 }
