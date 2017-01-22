7a18203b698a2b4c08f8c933387b210ae011fcd1
Loic Serafin <loicserafin@gmail.com>
2017-01-22 14:32:24 +0100

    can POST a user

--- src/main/java/ch/heigvd/api/ApplicationController.java
+++ src/main/java/ch/heigvd/api/ApplicationController.java
@@ -1,19 +1,20 @@
 package ch.heigvd.api;
 
 import ch.heigvd.Exception.ConflictException;
+import ch.heigvd.Exception.NotFoundException;
 import ch.heigvd.dao.ApplicationRepository;
 import ch.heigvd.dto.ApplicationDTO;
 import ch.heigvd.dto.ApplicationRegisterDTO;
 import ch.heigvd.models.Application;
+import ch.heigvd.models.Level;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiParam;
 import io.swagger.annotations.ApiResponse;
 import io.swagger.annotations.ApiResponses;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 /**
  * @author jfleroy
@@ -22,36 +23,42 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/applications")
 @Api(value = "auth", description = "the auth API")
-public class ApplicationController
-{
-    @Autowired
-    private ApplicationRepository applicationRepository;
+public class ApplicationController {
+	@Autowired
+	private ApplicationRepository applicationRepository;
 
-    @ApiResponses(value = {
-            @ApiResponse(
-                    code = 201,
-                    message = "Successful operation.",
-                    response = ApplicationDTO.class
-            ),
-            @ApiResponse(
-                    code = 409,
-                    message = "Application already registered",
-                    response = Void.class
-            )
-    })
+	@ApiResponses(value = {
+			@ApiResponse(
+					code = 201,
+					message = "Successful operation.",
+					response = ApplicationDTO.class
+			),
+			@ApiResponse(
+					code = 409,
+					message = "Application already registered",
+					response = Void.class
+			)
+	})
 
-    @RequestMapping(produces = {"application/json"}, method = RequestMethod.POST)
-    @ApiParam(value = "The information of the new application", required = true)
-    public ApplicationDTO post(@RequestBody ApplicationRegisterDTO input) {
-        Application app = applicationRepository.findByName(input.name);
-        if(app != null){
-            throw new ConflictException("Application already registered");
-        }
+	@RequestMapping(produces = {"application/json"}, method = RequestMethod.POST)
+	@ApiParam(value = "The information of the new application", required = true)
+	public ApplicationDTO post(@RequestBody ApplicationRegisterDTO input) {
+		Application app = applicationRepository.findByName(input.name);
+		if (app != null) {
+			throw new ConflictException("Application already registered");
+		}
 
-        app = input.buildApplication();
+		app = input.buildApplication();
 
-        applicationRepository.save(app);
+		applicationRepository.save(app);
 
-        return ApplicationDTO.fromApplication(app);
-    }
+		return ApplicationDTO.fromApplication(app);
+	}
+
+	@RequestMapping(method = RequestMethod.DELETE)
+	public ResponseEntity delete(@RequestAttribute("application") Application app) {
+		applicationRepository.delete(app);
+		return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
+	}
+
 }
