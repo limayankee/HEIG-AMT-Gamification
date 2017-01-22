67b8e3537d8a1ae78047ffbe77ea6d9599624744
Loic Serafin <loicserafin@gmail.com>
2017-01-22 13:32:54 +0100

    user endpoints tests

--- src/test/java/ch/heigvd/api/UserTest.java
+++ src/test/java/ch/heigvd/api/UserTest.java
@@ -1,57 +1,73 @@
 package ch.heigvd.api;
 
+import ch.heigvd.dto.EventDTO;
 import ch.heigvd.dto.UserDTO;
-import ch.heigvd.models.User;
+import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
-import org.springframework.http.HttpMethod;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
+import org.springframework.http.*;
 import org.springframework.test.context.junit4.SpringRunner;
-import org.springframework.test.web.client.MockRestServiceServer;
-import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.web.client.HttpClientErrorException;
-import org.springframework.web.client.RestTemplate;
 
 import static org.junit.Assert.*;
-import static org.springframework.test.web.client.match.MockRestRequestMatchers.method;
-import static org.springframework.test.web.client.match.MockRestRequestMatchers.requestTo;
-import static org.springframework.test.web.client.response.MockRestResponseCreators.withBadRequest;
 
 @RunWith(SpringRunner.class)
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.MOCK)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class UserTest {
+	private String mockUser = "mockUser";
+	private String mockTemporaryUser = "mockTemporaryUser";
+	private String mockInexistantUser = "mockInexstantUser";
 
 	@Autowired
-	private RestTemplate restTemplate;
-	private MockRestServiceServer mockServer;
+	private TestRestTemplate restTemplate;
 
 
 	@Before
 	public void setup() {
-		mockServer = MockRestServiceServer.createServer(restTemplate);
+		EventDTO mockEvent = new EventDTO("mockEvent", mockUser);
+		restTemplate = restTemplate.withBasicAuth("pollcat", "pollcat");
+		restTemplate.postForObject("/events", mockEvent, Void.class);
+	}
 
+	@After
+	public void cleanup() {
+		restTemplate.delete("/users/{userId}", mockUser);
 	}
 
 	@Test
-	public void testCannotGet() {
-		mockServer.expect(requestTo("/users/1"))
-				.andExpect(method(HttpMethod.GET))
-				.andRespond(withBadRequest());
-
-		try {
-			restTemplate.getForObject("/users/{userId}", UserDTO.class, 1);
-		} catch(HttpClientErrorException e) {
-			assertEquals(HttpStatus.BAD_REQUEST, e.getStatusCode());
-		}
-
-		mockServer.verify();
+	public void testUserNotFound() {
+		HttpEntity<Void> entity = new HttpEntity<>(null, new HttpHeaders());
+		HttpStatus response = restTemplate
+				.exchange("/users/{userId}", HttpMethod.GET, entity, UserDTO.class, mockInexistantUser)
+				.getStatusCode();
+		assertEquals(HttpStatus.NOT_FOUND, response);
 	}
 
+	@Test
+	public void testUserFound() {
+		UserDTO user = restTemplate.getForObject("/users/{userId}", UserDTO.class, mockUser);
+		assertEquals(user.getId(), mockUser);
+	}
+
+	@Test
+	public void testDeleteUser() {
+		EventDTO mockEvent = new EventDTO("mockEvent", mockTemporaryUser);
+		restTemplate.postForObject("/events", mockEvent, Void.class); //creates a new user
+
+		UserDTO user = restTemplate.getForObject("/users/{userId}", UserDTO.class, mockTemporaryUser);
+		assertEquals(user.getId(), mockTemporaryUser); //checks that user exists
+
+		restTemplate.delete("/users/{userId}", mockTemporaryUser);
+
+		HttpEntity<Void> entity = new HttpEntity<>(null, new HttpHeaders());
+		HttpStatus response = restTemplate //expects that user is NOT_FOUND
+				.exchange("/users/{userId}", HttpMethod.GET, entity, UserDTO.class, mockTemporaryUser)
+				.getStatusCode();
+		assertEquals(HttpStatus.NOT_FOUND, response);
+	}
 
 
 }
