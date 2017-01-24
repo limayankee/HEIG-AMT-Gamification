3998f5cd01a46ab6a2fb3859e81d99d2b28823a6
galedric <g@ledric.me>
2017-01-24 15:46:12 +0100

    Fix criterias

--- src/main/java/ch/heigvd/api/EventController.java
+++ src/main/java/ch/heigvd/api/EventController.java
@@ -3,16 +3,16 @@ package ch.heigvd.api;
 import ch.heigvd.dao.*;
 import ch.heigvd.dto.EventDTO;
 import ch.heigvd.dto.EventProcessingResultDTO;
-import ch.heigvd.dto.ScriptEngineResultDTO;
 import ch.heigvd.models.*;
 import ch.heigvd.scripting.rules.CriterionDelta;
 import ch.heigvd.scripting.rules.RuleEngine;
 import ch.heigvd.scripting.triggers.TriggerEngine;
-import io.swagger.annotations.ApiParam;
+import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiResponse;
 import io.swagger.annotations.ApiResponses;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+import springfox.documentation.annotations.ApiIgnore;
 
 import javax.validation.Valid;
 import java.util.Collection;
@@ -22,6 +22,7 @@ import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping(value = "/events", consumes = "application/json")
+@Api(value = "Events", description = "Events processing")
 @ApiResponses(value = {
 		@ApiResponse(
 				code = 401,
@@ -29,7 +30,6 @@ import java.util.stream.Collectors;
 		)
 })
 public class EventController {
-
 	@Autowired
 	private UserRepository userRepository;
 
@@ -51,21 +51,9 @@ public class EventController {
 	@Autowired
 	private TriggerRepository triggerRepository;
 
-	@ApiResponses(value = {
-			@ApiResponse(
-					code = 200,
-					message = "Successful operation.",
-					response = ScriptEngineResultDTO.class
-			),
-			@ApiResponse(
-					code = 400,
-					message = "Bad request for event",
-					response = Void.class
-			)
-	})
-	@ApiParam(value = "The information of the new application", required = true)
 	@RequestMapping(method = RequestMethod.POST)
-	EventProcessingResultDTO processEvent(@RequestAttribute("application") Application app, @Valid @RequestBody EventDTO event) throws Exception {
+	EventProcessingResultDTO processEvent(@ApiIgnore @RequestAttribute("application") Application app,
+	                                      @Valid @RequestBody EventDTO event) throws Exception {
 		User user = userRepository.findByAppUserIdAndApplicationId(event.getUserId(), app.getId());
 		if (user == null) {
 			user = new User(applicationRepository.findById(app.getId()), event.getUserId());
